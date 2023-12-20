cf7396f72ae5b9dbaa78d72aa8b4062547aaef91
Dave <dlrobjr@gmail.com>
2023-12-20 08:28:57 -0700

    displaying users on the webpage

--- public/app.js
+++ public/app.js
@@ -1,13 +1,27 @@
 let names = [];
 
+function buildNamesContainer(id, name){
+    //arr.forEach(el => {
+       $div = $("<div>").addClass(`user${id}`); 
+       $div.text(name);
+       $("container").append($div);
+    //})
+    
+    
+}
+
 async function getName(){
   await $.get('/api/users')
-    .then(data => data.forEach(element => {
-        names.push(element.name);
-    }))
+    .then(data => {
+        data.forEach(element => {
+        buildNamesContainer(element.id, element.name);
+    })
+})
     .fail(error => console.error('Error:', error))  
     console.log(names);
 }
+
+$(document)
 
 getName();
 
--- server.js
+++ server.js
@@ -14,10 +14,12 @@ db.connect();
 app.use(express.static('public'))
 //app.use(cors());
 app.use(express.json());
+
 app.get("/api/users", async (req, res)=>{
     try {
         console.log("received");
-        const {rows} = await db.query("SELECT name FROM users;")
+        const {rows} = await db.query("SELECT id, name FROM users;")
+        //console.log(results);
         res.send(rows);
       } catch (error) {
         console.error(error);
